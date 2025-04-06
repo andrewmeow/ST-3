8ea04887a0382463b1286a0321e23d91917c61cb
andrewmeow <a.zolotareva.2004@mail.ru>
2025-04-06 11:25:05 +0300

    add classes and tests

--- src/TimedDoor.cpp
+++ src/TimedDoor.cpp
@@ -1,2 +1,44 @@
 // Copyright 2021 GHA Test Team
 #include "TimedDoor.h"
+#include <chrono>
+#include <stdexcept>
+#include <thread>
+
+DoorTimerAdapter::DoorTimerAdapter(TimedDoor &d) : door(d) {}
+
+void DoorTimerAdapter::Timeout() { door.throwState(); }
+
+TimedDoor::TimedDoor(int timeout) : iTimeout(timeout), isOpened(false) {
+  adapter = new DoorTimerAdapter(*this);
+}
+
+TimedDoor::~TimedDoor() { delete adapter; }
+
+bool TimedDoor::isDoorOpened() { return isOpened; }
+
+void TimedDoor::unlock() {
+  isOpened = true;
+  Timer timer;
+  timer.tregister(iTimeout, adapter);
+}
+
+void TimedDoor::lock() { isOpened = false; }
+
+int TimedDoor::getTimeOut() const { return iTimeout; }
+
+void TimedDoor::throwState() {
+  if (isOpened) {
+    throw std::runtime_error("Door has been left open too long!");
+  }
+}
+
+void Timer::sleep(int milliseconds) {
+  std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
+}
+
+void Timer::tregister(int timeout, TimerClient *client) {
+  std::thread([client, timeout]() {
+    std::this_thread::sleep_for(std::chrono::seconds(timeout));
+    client->Timeout();
+  }).detach();
+}
--- test/tests.cpp
+++ test/tests.cpp
@@ -2,5 +2,88 @@
 
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
-#include <cstdint>
 #include "TimedDoor.h"
+
+class MockTimerClient : public TimerClient {
+public:
+  MOCK_METHOD(void, Timeout, (), (override));
+};
+
+// Фикстура тестов для ST-3
+class ST-3 : public ::testing::Test {
+protected:
+  TimedDoor *door;
+  MockTimerClient *mockClient;
+
+  void SetUp() override {
+    door = new TimedDoor(100); // Таймаут 100 мс
+    mockClient = new MockTimerClient();
+  }
+
+  void TearDown() override {
+    delete door;
+    delete mockClient;
+  }
+};
+
+// Проверка начального состояния двери (она должна быть закрыта)
+TEST_F(ST-3, InitialState) { EXPECT_FALSE(door->isDoorOpened()); }
+
+// Проверка открытия двери
+TEST_F(ST-3, OpenDoor) {
+  door->unlock();
+  EXPECT_TRUE(door->isDoorOpened());
+}
+
+// Проверка закрытия двери
+TEST_F(ST-3, CloseDoor) {
+  door->unlock();
+  door->lock();
+  EXPECT_FALSE(door->isDoorOpened());
+}
+
+// Проверка, что выбрасывается исключение при оставленной открытой двери
+TEST_F(ST-3, ExceptionOnTimeout) {
+  door->unlock();
+  std::this_thread::sleep_for(
+      std::chrono::milliseconds(150)); // Ждем больше таймаута
+  EXPECT_THROW(door->throwState(), std::runtime_error);
+}
+
+// Проверка, что нет исключения, если дверь была закрыта вовремя
+TEST_F(ST-3, NoExceptionWhenClosed) {
+  door->unlock();
+  door->lock();
+  std::this_thread::sleep_for(std::chrono::milliseconds(150));
+  EXPECT_NO_THROW(door->throwState());
+}
+
+// Проверка таймаута с мок-объектом
+TEST_F(ST-3, TimerTriggersTimeout) {
+  Timer timer;
+  EXPECT_CALL(*mockClient, Timeout()).Times(1);
+  timer.tregister(100, mockClient);
+}
+
+// Проверка работы DoorTimerAdapter
+TEST_F(ST-3, AdapterTriggersTimeout) {
+  DoorTimerAdapter adapter(*door);
+  EXPECT_THROW(adapter.Timeout(), std::runtime_error);
+}
+
+// Проверка метода getTimeOut
+TEST_F(ST-3, GetTimeoutValue) { EXPECT_EQ(door->getTimeOut(), 100); }
+
+// Проверка повторного открытия двери после закрытия
+TEST_F(ST-3, ReopenAfterClose) {
+  door->unlock();
+  door->lock();
+  door->unlock();
+  EXPECT_TRUE(door->isDoorOpened());
+}
+
+// Проверка повторного закрытия двери (должна оставаться закрытой)
+TEST_F(ST-3, RecloseDoor) {
+  door->lock();
+  EXPECT_FALSE(door->isDoorOpened());
+}
